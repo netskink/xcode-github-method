198483ea69660e90e217feb79289a3fc14f8a6ea
John F. Davis <davisjf@gmail.com>
2016-02-01 09:14:04 -0500

    Initial Commit

---
+++ testgame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testgame
+//
+//  Created by John Fred Davis on 2/1/16.
+//  Copyright © 2016 Netskink Computing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testgame/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  testgame
+//
+
+//  Copyright (c) 2016 Netskink Computing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
